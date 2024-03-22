3d81a35a1f8908e00dffb6b4d1af7c366037f4b6
Danmuumbi <muuomuumbi@gmail.com>
2024-03-22 18:47:53 +0300

    my initial commit

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,13 @@
+# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
+# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
+
